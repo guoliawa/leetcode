96ecf962a38f0e9becc587f3ccb9509c80de97e5
Guoliang Wang <guoliang_wang007@163.com>
2015-06-10 13:22:58 +0800

    New items.

---
+++ src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+	public boolean containsDuplicate(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return false;
+		}
+
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i : nums) {
+			if (!set.add(i)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
